81afc4b9578d739657b37ac2fb427e04336e347b
PetrRyb <petr.rybin@infis.cz>
2025-04-22 19:26:30 +0200

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -3,20 +3,16 @@ robot ={
  "R2D2":"opravář", 
 }
 hodnota = input("opravář") 
-klíč = input("Jaké je jméno robota?"):
-if klíč in robot:
+klíč = input("Jaké je jméno robota?")
+for klíč in robot.items:
      print(f"{hodnota}, {robot[hodnota]}")  
 else:
     Odpověď = input(" Robot není v seznamu chcete ho přidat do seznamu, jakožto nového robota?")
 
 if Odpověď == "ano":
     klíč = input("Zadejte název robota")
-
-for klíč in robot.items: 
+for klíč in robot: 
     print(robot)    
 
  while True:
-    klíčové_slovo = input( napište 'konec' pokavaď už nechcete přidávat robota či odpovídat na otázku ohledně jeho jména):
-   
-
-
+    klíčové_slovo = input( "Napište "konec" pokavaď už nechcete přidávat robota či odpovídat na otázku ohledně jeho jména"):
