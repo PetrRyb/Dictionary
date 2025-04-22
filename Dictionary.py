26d0aad64fce56dd3266b2b2c65044dc4ede6484
PetrRyb <petr.rybin@infis.cz>
2025-04-22 09:39:20 +0200

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+
+robot ={ 
+ "opravář":"R2D2", 
+}
+for robot in robot.items
+klíčovéSlovo = input("Jaké je jméno robota?"):
+if klíčovéSlovo in robot:
+    print(f"opravář")
+else:
+    Odpověď =("Chcete přidat nového robota?")
+
+if hodnota in robotiOdpověď == "ano":
+    klíčovéSlovo = input("Zadejte název robota")
+
+for robot in robot.items: 
+    print(robot)    
+
+    while
+   
+
+
